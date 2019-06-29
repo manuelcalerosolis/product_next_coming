ac78ac69201dc02fc5c2597a6f32c8dd615813f2
manuelcalerosolis <manuelcalerosolis@gmail.com>
2019-06-29 11:56:59 +0200

    comments

--- __manifest__.py
+++ __manifest__.py
@@ -7,15 +7,14 @@
     'name': "product_next_coming",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+        Show a column with the date of the next delivery of a product
+    """,
     'description': """
-        Long description of module's purpose
+        Show a column with the date of the next delivery of a product
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Xtendo",
+    'website': "http://www.xtendo.es",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
